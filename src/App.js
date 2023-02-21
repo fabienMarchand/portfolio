8beb029ec2d9e26f0126d181ac6e5749dcdcc1ff
Marcha <marchaf@gmail.com>
2023-02-21 18:36:40 +0100

    add styles and responsivness

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-const hamburgerButton = document.getElementById('hamburger')
-const navList = document.getElementById('nav-list')
+const hamburgerButton = document.getElementById("hamburger");
+const navList = document.getElementById("nav-list");
 
-function toggleButton() {
-    navList.classList.toggle('show')
-}
+// function toggleButton() {
+//   navList.classList.toggle("show");
+// }
 
-hamburgerButton.addEventListener('click', toggleButton)
+// hamburgerButton.addEventListener("click", toggleButton);
+
+hamburgerButton.onclick = () => {
+  hamburgerButton.classList.toggle("open");
+  navList.classList.toggle("slide")
+};
+
+const navbar = document.querySelector(".navbar");
+const bar = document.getElementById("bar");
+window.onscroll = () => {
+  if (window.scrollY >= 980) {
+    navbar.classList.add("nav-active");
+  } else {
+    navbar.classList.remove("nav-active");
+  }
+};
