e81043f1277ebced2cf5341cb40b5d2100e40094
Marcha <marchaf@gmail.com>
2023-02-21 11:50:04 +0100

    add elem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+const hamburgerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function toggleButton() {
+    navList.classList.toggle('show')
 }
 
-export default App;
+hamburgerButton.addEventListener('click', toggleButton)
